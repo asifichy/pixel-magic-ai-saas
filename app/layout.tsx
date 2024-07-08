56e77a54d90170e27074a6a7e16479ab8a7b86e0
asifichy <asifimtiyajchowdhury@gmail.com>
2024-07-09 00:03:12 +0600

    setting appearence in the auth page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
         {children}
