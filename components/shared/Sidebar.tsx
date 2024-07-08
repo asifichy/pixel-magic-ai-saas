979d571c6a6d1e13314acddc97d949106a267c0a
asifichy <asifimtiyajchowdhury@gmail.com>
2024-07-09 00:32:12 +0600

    adding constant files in a folder to use frequently

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,8 +1,41 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import { SignedIn } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
+import { navLinks } from '@/constants'
 
 const Sidebar = () => {
+
+    const pathname = usePathname();
+
   return (
-    <div>Sidebar</div>
+    <aside className='sidebar'>
+        <div className="flex size-full flex-col gap-4">
+            <Link href="/" className="sidebar-logo">
+                <Image src="/assets/images/Pixel Magic.png" alt="Logo" width={180} height={10}/>
+            </Link>
+
+            <nav className='sidebar-nav'>
+                <SignedIn>
+                    <ul className='sidebar-nav_elements'>
+                        {navLinks.map((link) => {
+                            const isActive = link.route === pathname;
+
+                            return (
+                                <li key={link.route} className={`sidebar-nav_element group 
+                                    ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </SignedIn>
+            </nav>
+
+        </div>        
+
+    </aside> // it's a html5 symantic tag means something on the side
   )
 }
 
