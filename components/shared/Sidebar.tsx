d6b170a2a7432898ebd2909b74c2857e64a94c4a
asifichy <asifimtiyajchowdhury@gmail.com>
2024-07-14 21:33:25 +0600

    sidebar nav completes

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -3,9 +3,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { usePathname } from 'next/navigation'
 import { navLinks } from '@/constants'
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
 
@@ -21,7 +22,7 @@ const Sidebar = () => {
             <nav className='sidebar-nav'>
                 <SignedIn>
                     <ul className='sidebar-nav_elements'>
-                        {navLinks.map((link) => {
+                        {navLinks.slice(0, 6).map((link) => {
                             const isActive = link.route === pathname;
 
                             return (
@@ -30,17 +31,39 @@ const Sidebar = () => {
                                         <Link className="sidebar-link" href={link.route}>
                                             <Image src={link.icon} alt="logo" width={24} height={20}
                                                 className={`${isActive &&'brightness-200'}`}/>
-
                                                 {link.label}
                                         </Link>
                                 </li>
                             )
                         })}
                     </ul>
+                    
+                    <ul className='sidebar-nav_elements'>
+                        {navLinks.slice(6).map((link) => {
+                            const isActive = link.route === pathname;
+
+                            return (
+                                <li key={link.route} className={`sidebar-nav_element group 
+                                    ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                                        <Link className="sidebar-link" href={link.route}>
+                                            <Image src={link.icon} alt="logo" width={24} height={20}
+                                                className={`${isActive &&'brightness-200'}`}/>
+                                                {link.label}
+                                        </Link>
+                                </li>
+                            )
+                        })}
+
+                        <li className="flex-center cursor-pointer gap-2 p-4">
+                            <UserButton afterSignOutUrl='/' showName/>
+                        </li>
+                    </ul>
                 </SignedIn>
 
                 <SignedOut>
-                    
+                    <Button asChild className="buton bg-purple-gradient bg-cover">
+                        <Link href="/sign-in">Login</Link>
+                    </Button>
                 </SignedOut>
             </nav>
 
